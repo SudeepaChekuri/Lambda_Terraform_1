7fec247609ac916d7c83a25d6b0568c31ea7df52
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-14 21:53:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,11 @@ resource "aws_iam_role" "lambda_role" {
 }
 EOF
 }
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "lambda_s3_bucket"
+  # Add other configuration options for the bucket, if needed
+}
+
 
 resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   name       = "lambda_policy_attachment"
@@ -37,8 +42,8 @@ resource "aws_lambda_function" "example_lambda_function" {
   #source_code_hash = filebase64sha256("example_lambda_function.py")
 
   # Use S3 bucket as the deployment package source
-  s3_bucket        = var.lambda_s3_bucket
-  s3_key           = var.lambda_s3_key
+  s3_bucket        = aws_s3_bucket.example_bucket.bucket
+  s3_key           = "example_lambda_function.zip"
 
   environment {
     variables = {
